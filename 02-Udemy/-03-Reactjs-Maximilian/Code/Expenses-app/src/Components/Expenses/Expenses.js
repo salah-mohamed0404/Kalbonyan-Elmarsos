beaa0cdf279c3f71ccb0c7c19ab406f6b4aef2ce
salahcst344 <salahcst344@gmail.com>
2022-08-01 17:21:24 +0200

    dynamic JSX

--- 02-Udemy/-03-Reactjs-Maximilian/Code/Expenses-app/src/Components/Expenses/Expenses.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Code/Expenses-app/src/Components/Expenses/Expenses.js
@@ -17,26 +17,14 @@ function Expenses(props) {
           selected={filtteredYear}
           onSelected={selectYearHandler}
         />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        {/* dynamic JSX */}
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
