ad9749eb4e23f6e1916a0661b76818c75737ea2c
salahcst344 <salahcst344@gmail.com>
2022-08-05 19:43:55 +0200

    Build the cart

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/order-food-app/S11-project/src/Components/Layout/HeaderCartButton.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/order-food-app/S11-project/src/Components/Layout/HeaderCartButton.js
@@ -3,7 +3,7 @@ import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = function (props) {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
