8707b172ce60f9eb655949741f0201e1ebabb30f
salahcst344 <salahcst344@gmail.com>
2022-08-07 13:48:09 +0200

    Get movies and manging it

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/movies-app/src/App.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/movies-app/src/App.js
@@ -1,37 +1,58 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = async function () {
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
+  const fetchMoviesHandler = useCallback(async function () {
+    try {
+      setIsLoading(true);
+      setError(null);
 
-    const transformedMovies = data.results.map(MovieData => {
-      return {
-        id: MovieData.episode_id,
-        title: MovieData.title,
-        openingText: MovieData.opening_crawl,
-        releaseDate: MovieData.release_date,
-      };
-    });
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) throw new Error("Something went wrong");
+      const data = await response.json();
 
-    setMovies(transformedMovies);
-  };
+      const transformedMovies = data.results.map(MovieData => {
+        return {
+          id: MovieData.episode_id,
+          title: MovieData.title,
+          openingText: MovieData.opening_crawl,
+          releaseDate: MovieData.release_date,
+        };
+      });
+
+      setMovies(transformedMovies);
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(
+    function () {
+      fetchMoviesHandler();
+    },
+    [fetchMoviesHandler]
+  );
+
+  let content = <p>Found no movies.</p>;
+  if (movies.length > 0) content = <MoviesList movies={movies} />;
+  if (error) content = <p>{error}</p>;
+  if (isLoading) content = <p>Loading...</p>;
+
+  return (
+    <>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </>
+  );
 }
-
-return (
-  <>
-    <section>
-      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-    </section>
-    <section>
-      <MoviesList movies={movies} />
-    </section>
-  </>
-);
 
 export default App;
