ea02bdf768c86e6497ca9a530cc2e763d0af5df7
salahcst344 <salahcst344@gmail.com>
2022-08-09 19:18:38 +0200

    Working with muliple state

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/redux-counter/src/components/Header.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/redux-counter/src/components/Header.js
@@ -1,22 +1,34 @@
-import classes from './Header.module.css';
+import { useDispatch } from "react-redux";
+import { authActions } from "../store/index";
+import { useSelector } from "react-redux";
+import classes from "./Header.module.css";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const logout = e => {
+    e.preventDefault();
+    dispatch(authActions.logout());
+  };
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logout}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
