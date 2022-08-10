7e63871a0aab0fb279e2f5218c7d3204784dfb38
salahcst344 <salahcst344@gmail.com>
2022-08-10 19:29:12 +0200

    Quote details in action

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/quotes-app/src/pages/NewQuote.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/quotes-app/src/pages/NewQuote.js
@@ -1,3 +1,11 @@
-const NewQuote = () => {};
+import QuoteForm from "../components/quotes/QuoteForm";
+
+const NewQuote = () => {
+  const addQuoteHandler = function (qupteData) {
+    console.log(qupteData);
+  };
+
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
+};
 
 export default NewQuote;
--- 02-Udemy/-03-Reactjs-Maximilian/Projects/quotes-app/src/pages/QuoteDetail.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/quotes-app/src/pages/QuoteDetail.js
@@ -1,12 +1,25 @@
 import { Route, useParams } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Salah", text: "Learning React is fun!" },
+  { id: "q2", author: "Salah Mohamed", text: "Learning React is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find(qoute => qoute.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote found</p>;
+  }
+
   return (
     <>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
