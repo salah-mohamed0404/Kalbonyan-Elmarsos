d272bb89faa0042e955de7603c50d0b646eb6392
salahcst344 <salahcst344@gmail.com>
2022-08-10 13:35:58 +0200

    useEffect to send http request

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/redux-cart/src/App.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/redux-cart/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -5,6 +6,16 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(
+    () =>
+      fetch("https://cart-5ff9c-default-rtdb.firebaseio.com/cart.json", {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }),
+    [cart]
+  );
 
   return (
     <Layout>
