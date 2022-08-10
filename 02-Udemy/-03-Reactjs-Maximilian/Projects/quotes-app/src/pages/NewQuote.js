4ffbebd7ab143de20e9a1761fe6f25b83e28db59
salahcst344 <salahcst344@gmail.com>
2022-08-10 19:33:18 +0200

    Programatic navigation

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/quotes-app/src/pages/NewQuote.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/quotes-app/src/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = function (qupteData) {
     console.log(qupteData);
+
+    history.push("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
