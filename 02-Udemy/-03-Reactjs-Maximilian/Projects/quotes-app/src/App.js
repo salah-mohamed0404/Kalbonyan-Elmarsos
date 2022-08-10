e503ad22d33c3341a39caa41eaeaa1099d82c29b
salahcst344 <salahcst344@gmail.com>
2022-08-10 19:00:41 +0200

    Add navigation

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/quotes-app/src/App.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/quotes-app/src/App.js
@@ -1,9 +1,32 @@
-function App() {
-  return (
-    <div>
+import { Redirect, Route, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 
-    </div>
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
+const App = () => {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
-}
+};
 
 export default App;
