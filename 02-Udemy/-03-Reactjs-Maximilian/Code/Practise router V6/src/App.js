dd1057e3a880c9e1054ba378d77e4da12d5ad3b4
salahcst344 <salahcst344@gmail.com>
2022-08-11 14:15:48 +0200

    Update to router V6

--- 02-Udemy/-03-Reactjs-Maximilian/Code/Practise router V6/src/App.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Code/Practise router V6/src/App.js
@@ -1,33 +1,29 @@
-import { Route, Switch } from 'react-router-dom';
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import MainHeader from './components/MainHeader';
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Switch>
-          <Route path='/welcome'>
-            <Welcome />
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/welcome" />} />
+
+          <Route path="/welcome/*" element={<Welcome />}>
+            <Route path="new-user" element={<p>Welcome, new user</p>} />
           </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
+
+          <Route path="/products" element={<Products />} />
+
+          <Route path="/products/:productId" element={<ProductDetail />} />
+        </Routes>
       </main>
     </div>
   );
 }
 
 export default App;
-
-// our-domain.com/welcome => Welcome Component
-// our-domain.com/products => Products Component
-// our-domain.com/product-detail/a-book
