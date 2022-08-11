72a406736fc401c4e0d02d0f05209295c9f8352b
salahcst344 <salahcst344@gmail.com>
2022-08-11 18:41:39 +0200

    Implement auth in forms

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/Authentication-page/src/components/Profile/ProfileForm.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/Authentication-page/src/components/Profile/ProfileForm.js
@@ -1,17 +1,55 @@
-import classes from './ProfileForm.module.css';
+import { useContext, useRef } from "react";
+import classes from "./ProfileForm.module.css";
+import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
 
 const ProfileForm = () => {
+  const history = useHistory();
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = e => {
+    e.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    if (enteredNewPassword < 6) return console.log("invalid new pass");
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDrMMYxdSLIlv4bRM-WxyIQVXhhQh3V4xg",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(res => {
+      // assime it's always successed
+      history.replace("/");
+    });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          minLength="7"
+          ref={newPasswordInputRef}
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
