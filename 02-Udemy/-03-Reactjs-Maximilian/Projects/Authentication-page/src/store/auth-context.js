3924b9aa85f3768adb0101940eb3975af0435a0f
salahcst344 <salahcst344@gmail.com>
2022-08-11 19:00:11 +0200

    Keep log in state

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/Authentication-page/src/store/auth-context.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/Authentication-page/src/store/auth-context.js
@@ -8,12 +8,19 @@ const AuthContext = createContext({
 });
 
 export const AuthContextProvider = props => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
 
-  const loginHandler = token => setToken(token);
-  const logoutHandler = () => setToken(null);
+  const loginHandler = token => {
+    setToken(token);
+    localStorage.setItem("token", token);
+  };
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
 
   const contextValue = {
     token: token,
