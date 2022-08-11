39f5a82bb17525f6bfee99a4c0693f69338808f7
salahcst344 <salahcst344@gmail.com>
2022-08-11 14:46:09 +0200

    Implementing lazy loading to pages

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/quotes-app/src/App.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/quotes-app/src/App.js
@@ -1,35 +1,46 @@
+import { lazy, Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import Layout from "./components/layout/Layout";
 
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
-import QuoteDetail from "./pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = lazy(() => import("./pages/NewQuote"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const QuoteDetail = lazy(() => import("./pages/QuoteDetail"));
+const AllQuotes = lazy(() => import("./pages/AllQuotes"));
 
 const App = () => {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
 
-        <Route path="/quotes" exact>
-          <AllQuotes />
-        </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
 
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
 
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
 
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 };
