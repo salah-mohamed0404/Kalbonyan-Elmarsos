8f492eb96a7d923c9f93af4d946da928a42d488b
salahcst344 <salahcst344@gmail.com>
2022-08-11 21:21:44 +0200

    Refactor the animation  with react-transition-group package

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/animation-app/src/App.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/animation-app/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
+import { Transition } from "react-transition-group";
 
 class App extends Component {
   state = {
@@ -22,11 +23,16 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal
-          show={this.state.modalIsOpen}
-          closed={this.closeModal.bind(this)}
-        />
-        <Backdrop show={this.state.modalIsOpen} />
+        <Transition
+          in={this.state.modalIsOpen}
+          timeout={300}
+          mountOnEnter
+          unmountOnExit
+        >
+          {state => <Modal show={state} closed={this.closeModal.bind(this)} />}
+        </Transition>
+
+        {this.state.modalIsOpen && <Backdrop show />}
         <button className="Button" onClick={this.showModal.bind(this)}>
           Open Modal
         </button>
--- 02-Udemy/-03-Reactjs-Maximilian/Projects/animation-app/src/components/Modal/Modal.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/animation-app/src/components/Modal/Modal.js
@@ -3,7 +3,15 @@ import React from "react";
 import "./Modal.css";
 
 const modal = props => (
-  <div className={`Modal ${props.show ? "ModalOpen" : "ModalClosed"}`}>
+  <div
+    className={`Modal ${
+      props.show === "entering"
+        ? "ModalOpen"
+        : props.show === "exiting"
+        ? "ModalClosed"
+        : ""
+    }`}
+  >
     <h1>A Modal</h1>
     <button className="Button" onClick={props.closed}>
       Dismiss
