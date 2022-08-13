f1343eebd77e26e8618b39ad6feef97c01c63527
salahcst344 <salahcst344@gmail.com>
2022-08-13 14:43:44 +0200

    Build MeetupDetail page

---
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/meetsup-app/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://thumbs.dreamstime.com/z/nuture-green-tree-nice-128219037.jpg"
+      title="First meetup"
+      address="Some address 5, Some City"
+      description="he meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
