5b40bed83e2f151c65ae39388b716e6f18f56c22
salahcst344 <salahcst344@gmail.com>
2022-08-13 15:24:54 +0200

    use getStaticPaths

--- 02-Udemy/-03-Reactjs-Maximilian/Projects/meetsup-app/pages/[meetupId]/index.js
+++ 02-Udemy/-03-Reactjs-Maximilian/Projects/meetsup-app/pages/[meetupId]/index.js
@@ -1,14 +1,49 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://thumbs.dreamstime.com/z/nuture-green-tree-nice-128219037.jpg"
-      title="First meetup"
-      address="Some address 5, Some City"
-      description="he meetup description"
+      image={props.MeetupData.image}
+      title={props.MeetupData.title}
+      address={props.MeetupData.address}
+      description={props.MeetupData.description}
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      MeetupData: {
+        image:
+          "https://thumbs.dreamstime.com/z/nuture-green-tree-nice-128219037.jpg",
+        id: meetupId,
+        title: "First meetup",
+        address: "Some address 5, Some City",
+        description: "he meetup description",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
