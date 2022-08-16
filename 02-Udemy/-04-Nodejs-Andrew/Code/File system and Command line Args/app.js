bcdd007eca1d54f9f1f0b1f75c934eb455c61e4b
salahcst344 <salahcst344@gmail.com>
2022-08-16 22:00:53 +0200

    Practising with yargs and coding challenge

--- 02-Udemy/-04-Nodejs-Andrew/Code/File system and Command line Args/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/File system and Command line Args/app.js
@@ -1,9 +1,44 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-console.log(chalk.bold.green("Success"));
+// Customize yargs version
+yargs.version("1.1.0");
 
-const notes = getNotes();
-console.log(notes);
+// Create add command
+yargs.command({
+  command: "add",
+  descripe: "Add new note",
+  handler: function () {
+    console.log("adding a new note");
+  },
+});
 
-console.log(process.argv);
+// Create remove command
+yargs.command({
+  command: "remove",
+  descripe: "Remove note",
+  handler: function () {
+    console.log("Removing note");
+  },
+});
+
+// Challenge: implement two commands
+// Create list command
+yargs.command({
+  command: "list",
+  descripe: "list notes",
+  handler: function () {
+    console.log("Listing notes");
+  },
+});
+// Create read command
+yargs.command({
+  command: "read",
+  descripe: "read notes",
+  handler: function () {
+    console.log("Reading notes");
+  },
+});
+
+console.log(yargs.argv);
