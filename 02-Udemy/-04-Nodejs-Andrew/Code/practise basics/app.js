bcca0d2c32f2980ea44c31ee1d59bf67830fb52f
salahcst344 <salahcst344@gmail.com>
2022-08-16 19:10:58 +0200

    Use validator packege

--- 02-Udemy/-04-Nodejs-Andrew/Code/practise basics/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/practise basics/app.js
@@ -1,9 +1,5 @@
-const add = require("./utils.js");
-
-const sum = add(4, -2);
-
-console.log(sum);
-
+const validator = require("validator");
+console.log(validator.isURL("https://mead.io"));
 // Challenge
 /*
 const getNotes = require("./notes.js");
@@ -12,3 +8,9 @@ const notes = getNotes();
 
 console.log(notes);
 */
+
+// const add = require("./utils.js");
+
+// const sum = add(4, -2);
+
+// console.log(sum);
