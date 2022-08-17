b49860e61349902a40c534ee9ee1085a06cec483
salahcst344 <salahcst344@gmail.com>
2022-08-17 15:19:11 +0200

    Get start with json files

---
+++ 02-Udemy/-04-Nodejs-Andrew/Code/playground/1-json.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+// const book = {
+//   title: "Ego is the Enemy",
+//   auther: "Ryan Holiday",
+// };
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync("1-json.json", bookJSON);
+
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data);
