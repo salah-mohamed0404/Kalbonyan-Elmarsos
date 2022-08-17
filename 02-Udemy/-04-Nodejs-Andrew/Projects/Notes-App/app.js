b3d6774bbc1a008c09fb7a29ff9cc5a5bd3cf7d4
salahcst344 <salahcst344@gmail.com>
2022-08-17 16:45:00 +0200

    adding addNote functionallty and Coding challenge

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Notes-App/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Notes-App/app.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
-const getNotes = require("./notes.js");
+const notes = require("./notes.js");
 
 // Customize yargs version
 yargs.version("1.1.0");
@@ -22,8 +22,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log(`Title: ${argv.title}`);
-    console.log(`Body: ${argv.body}`);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
@@ -31,8 +30,15 @@ yargs.command({
 yargs.command({
   command: "remove",
   descripe: "Remove note",
-  handler: function () {
-    console.log("Removing note");
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title);
   },
 });
 
--- 02-Udemy/-04-Nodejs-Andrew/Projects/Notes-App/notes.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Notes-App/notes.js
@@ -1,3 +1,53 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+const loadNotes = function () {
+  try {
+    return JSON.parse(fs.readFileSync("notes.json").toString());
+  } catch (e) {
+    return [];
+  }
+};
+
+const saveNotes = (notes) =>
+  fs.writeFileSync("notes.json", JSON.stringify(notes));
+
 const getNotes = () => "Your Notes.....";
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  const duplicate = notes.find((note) => note.title === title);
+
+  if (duplicate) {
+    console.log(chalk.red("New title taken!"));
+    return;
+  }
+
+  notes.push({
+    title,
+    body,
+  });
+
+  saveNotes(notes);
+  console.log(chalk.green("New note added!"));
+};
+
+// Challenge implement remove function
+const removeNote = function (title) {
+  const notes = loadNotes();
+  const filteredNotes = notes.filter((note) => note.title !== title);
+
+  if (filteredNotes.length === notes.length) {
+    console.log(chalk.red("Title not even added"));
+    return;
+  }
+
+  saveNotes(filteredNotes);
+  console.log(chalk.green(`${title}'s note has been removed`));
+};
+
+module.exports = {
+  getNotes,
+  addNote,
+  removeNote,
+};
