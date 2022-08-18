73b748ac22c742d88ab56466973596d843a7ebc2
salahcst344 <salahcst344@gmail.com>
2022-08-18 19:47:05 +0200

    Combine geocode and forecast

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Weather-App/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Weather-App/app.js
@@ -1,38 +1,13 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-// const url =
-// "http://api.weatherstack.com/current?access_key=7db02b1df2ad734bf5df11961b5b4f8c&query=37.8267,-122.4233&units=f";
+geocode("cairo", function (error, data) {
+  if (error) return console.log(error);
 
-// request({ url, json: true }, function (error, response) {
-//   if (response?.body?.error) {
-//     console.log("Unable to find location");
-//   }
-//   else if (!error) {
-//     console.log(
-//       `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out, There is ${response.body.current.feelslike} chance of rain.`
-//     );
-//   }  else {
-//     console.log("Unable to connect to weather service!");
-//   }
-// });
+  forecast(data.lat, data.long, (error, forecastData) => {
+    if (error) return console.log(error);
 
-// geocode("cairo", function (error, data) {
-//   console.log(`Error ${error}`);
-//   console.log(`Data ${data}`);
-// });
-
-//Challenge
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-forecast(-25, 45, (error, data) => {
-  console.log("Error", error);
-  console.log("Data", data);
+    console.log(data.location);
+    console.log(forecastData);
+  });
 });
