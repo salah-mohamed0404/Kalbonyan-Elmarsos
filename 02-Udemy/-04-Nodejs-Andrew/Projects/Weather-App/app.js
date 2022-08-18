caf3b96bc2cfcfb7156efc626faae74478208cf1
salahcst344 <salahcst344@gmail.com>
2022-08-18 16:04:02 +0200

    Weather api error handle

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Weather-App/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Weather-App/app.js
@@ -4,7 +4,22 @@ const url =
   "http://api.weatherstack.com/current?access_key=7db02b1df2ad734bf5df11961b5b4f8c&query=37.8267,-122.4233&units=f";
 
 request({ url, json: true }, function (error, response) {
-  console.log(
-    `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out, There is ${response.body.current.feelslike} chance of rain.`
-  );
+  if (!error) {
+    console.log(
+      `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out, There is ${response.body.current.feelslike} chance of rain.`
+    );
+  } else if (response.body.error) {
+    console.log("Unable to find location");
+  } else {
+    console.log("Unable to connect to weather service!");
+  }
 });
+
+// const geocodeURL =
+//   "http://api.positionstack.com/v1/forward?access_key=1f890ce54fb793f478d3dcda4ed4a85a&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC&output=json&limit=1";
+
+// request({ url: geocodeURL, json: true }, function (error, response) {
+//   const lat = response.body.data[0].latitude;
+//   const long = response.body.data[0].longitude;
+//   console.log(lat, long);
+// });
