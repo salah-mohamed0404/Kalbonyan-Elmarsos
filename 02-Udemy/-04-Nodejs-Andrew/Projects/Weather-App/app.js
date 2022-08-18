1bbe28c7d97f7fd9378d869a2558050f6d52a235
salahcst344 <salahcst344@gmail.com>
2022-08-18 14:48:09 +0200

    Change unit and add description

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Weather-App/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Weather-App/app.js
@@ -1,10 +1,10 @@
 const request = require("request");
 
 const url =
-  "http://api.weatherstack.com/current?access_key=7db02b1df2ad734bf5df11961b5b4f8c&query=37.8267,-122.4233";
+  "http://api.weatherstack.com/current?access_key=7db02b1df2ad734bf5df11961b5b4f8c&query=37.8267,-122.4233&units=f";
 
 request({ url, json: true }, function (error, response) {
   console.log(
-    `It is currently ${response.body.current.temperature} degrees out, There is ${response.body.current.feelslike} chance of rain.`
+    `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out, There is ${response.body.current.feelslike} chance of rain.`
   );
 });
