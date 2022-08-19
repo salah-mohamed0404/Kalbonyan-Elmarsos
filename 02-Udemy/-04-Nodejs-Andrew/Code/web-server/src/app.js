6b8492b17a4a6cbd5ead3faa0a7c3d4ffa49d255
salahcst344 <salahcst344@gmail.com>
2022-08-19 16:33:04 +0200

    Explore path tool

--- 02-Udemy/-04-Nodejs-Andrew/Code/web-server/src/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/web-server/src/app.js
@@ -1,8 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.get("", (req, res) => res.send("<h1>Weather</h1>"));
+app.use(express.static(publicDirectoryPath));
 
 app.get("/help", (req, res) =>
   res.send([
