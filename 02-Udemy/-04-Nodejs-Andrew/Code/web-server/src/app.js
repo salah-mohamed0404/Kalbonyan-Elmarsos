0110b398e3184f016f8ee1876d8a27e120a876c9
salahcst344 <salahcst344@gmail.com>
2022-08-19 17:08:33 +0200

    Explore the power of hbs

--- 02-Udemy/-04-Nodejs-Andrew/Code/web-server/src/app.js
+++ 02-Udemy/-04-Nodejs-Andrew/Code/web-server/src/app.js
@@ -4,7 +4,22 @@ const express = require("express");
 const app = express();
 const publicDirectoryPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) =>
+  res.render("index", {
+    title: "Weather App",
+    name: "Salah",
+  })
+);
+
+app.get("/about", (req, res) =>
+  res.render("about", {
+    title: "About me",
+    name: "Salah Mohamed",
+  })
+);
 
 app.get("/weather", (req, res) =>
   res.send({
