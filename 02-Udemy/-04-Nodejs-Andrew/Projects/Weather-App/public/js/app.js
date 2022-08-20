00824d4532c83bde58007d10848e0da87c11a5ec
salahcst344 <salahcst344@gmail.com>
2022-08-20 17:19:17 +0200

    Release

---
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Weather-App/public/js/app.js
@@ -0,0 +1,25 @@
+console.log("Client side JS file is louded!");
+
+const form = document.querySelector("form");
+const search = document.querySelector("input");
+const messageParagraph = document.querySelector("#response");
+
+const setMessage = function (messageField, resMessage) {
+  messageField.textContent = "";
+  messageField.textContent = resMessage;
+};
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const location = search.value;
+
+  setMessage(messageParagraph, "Loudind...");
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.error) return setMessage(messageParagraph, data.error);
+
+      setMessage(messageParagraph, `${data.location}: ${data.forecast}`);
+    });
+});
