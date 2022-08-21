80fe17caf96ce8bc488b23dbc14cba3254ab67e1
salahcst344 <salahcst344@gmail.com>
2022-08-21 17:54:39 +0200

    Coding challenge

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/mongodb.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/mongodb.js
@@ -11,9 +11,57 @@ MongoClient.connect(
     if (error) return console.log("Unable to connect to database");
 
     const db = client.db(databaseName);
-    db.collection("users").insertOne({
-      name: "Salah",
-      age: 18,
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Salah",
+    //     age: 18,
+    //   },
+    //   (error, result) => {
+    //     if (error) return console.log("Unable to insert user");
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Jen",
+    //       age: 28,
+    //     },
+    //     {
+    //       name: "Gunther",
+    //       age: 27,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) return console.log("Unable to insert user");
+
+    //     console.log(result.insertedCount);
+    //   }
+    // );
+
+    // Challenge
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "See the course",
+          completed: true,
+        },
+        {
+          description: "Having fun",
+          completed: false,
+        },
+        {
+          description: "Set with family",
+          completed: true,
+        },
+      ],
+      function (error, result) {
+        if (error) return console.log("Unable to insert user");
+
+        console.log(result.insertedCount);
+      }
+    );
   }
 );
