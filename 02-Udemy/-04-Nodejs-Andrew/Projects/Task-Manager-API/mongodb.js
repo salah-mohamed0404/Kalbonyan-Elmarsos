5fb83abe2a3013d208c496eff99299af38ce4578
salahcst344 <salahcst344@gmail.com>
2022-08-22 15:36:18 +0200

    Explore updateOne

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/mongodb.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/mongodb.js
@@ -11,43 +11,18 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db.collection("users").findOne(
-    //   { _id: new ObjectID("630252b74cac67e599e5288c") },
-    //   function (error, user) {
-    //     if (error) return console.log("Unable to fetch user");
-
-    //     console.log(user);
-    //   }
-    // );
-
-    // db.collection("users")
-    //   .find({ age: 18 })
-    //   .toArray(function (error, users) {
-    //     console.log(users);
-    //   });
-
-    // db.collection("users")
-    //   .find({ age: 18 })
-    //   .count(function (error, count) {
-    //     console.log(count);
-    //   });
-
-    // Coding challenge on find and findOne
-    db.collection("tasks").findOne(
-      { _id: new ObjectID("6302543a6c5603bbae34a29a") },
-      function (error, lastTask) {
-        if (error) return console.log("Unable to fetch last task");
-
-        console.log(lastTask);
-      }
-    );
-
-    db.collection("tasks")
-      .find({ completed: false })
-      .toArray(function (error, uncompleteTasks) {
-        if (error) return console.log("Unable to fetch tasks");
-
-        console.log(uncompleteTasks);
-      });
+    db.collection("users")
+      .updateOne(
+        {
+          _id: ObjectId("63024c0f77e06fe48f86f3d9"),
+        },
+        {
+          $inc: {
+            age: 1,
+          },
+        }
+      )
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
   }
 );
