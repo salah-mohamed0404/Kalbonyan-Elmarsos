2caf569ddb8eaf480b5b922f6da5e053e6231d05
salahcst344 <salahcst344@gmail.com>
2022-08-23 13:46:09 +0200

    Explore mongoose

---
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/db/mongoose.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+const me = new User({
+  name: "Salah",
+  age: 18,
+});
+
+me.save()
+  .then(() => console.log(me))
+  .catch((err) => console.log("Error", err));
