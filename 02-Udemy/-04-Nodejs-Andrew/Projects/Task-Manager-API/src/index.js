2e3efe6cf87d9ec51a68e4fd977fd092ef537b02
salahcst344 <salahcst344@gmail.com>
2022-08-23 17:31:16 +0200

    Add post request

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/index.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post("/users", function (req, res) {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => res.send(user))
+    .catch((err) => res.status(400).send(err));
+});
 
 app.listen(port, () => console.log(`Server is up on port ${port}`));
