5b7e725b14d2cd81b717ab43839cee9cacda0a3b
salahcst344 <salahcst344@gmail.com>
2022-08-23 19:02:23 +0200

    Coding challenge

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/index.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/index.js
@@ -1,6 +1,7 @@
-const express = require("express");
 require("./db/mongoose");
+const express = require("express");
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +13,16 @@ app.post("/users", function (req, res) {
 
   user
     .save()
-    .then(() => res.send(user))
+    .then(() => res.status(201).send(user))
+    .catch((err) => res.status(400).send(err));
+});
+
+app.post("/tasks", function (req, res) {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => res.status(201).send(task))
     .catch((err) => res.status(400).send(err));
 });
 
---
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/models/task.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  combleted: { type: Boolean, default: false },
+});
+
+module.exports = Task;
