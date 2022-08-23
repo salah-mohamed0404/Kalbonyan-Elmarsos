afc2b4c0e28cfec9b5316fe4c78c15edf7d35031
salahcst344 <salahcst344@gmail.com>
2022-08-23 13:54:43 +0200

    Coding challenge

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/db/mongoose.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/db/mongoose.js
@@ -14,11 +14,29 @@ const User = mongoose.model("User", {
   },
 });
 
-const me = new User({
-  name: "Salah",
-  age: 18,
+// const me = new User({
+//   name: "Salah",
+//   age: 18,
+// });
+
+// me.save()
+//   .then(() => console.log(me))
+//   .catch((err) => console.log("Error", err));
+
+// Challenge
+const Task = mongoose.model("Tasks", {
+  description: {
+    type: String,
+  },
+  combleted: Boolean,
 });
 
-me.save()
-  .then(() => console.log(me))
-  .catch((err) => console.log("Error", err));
+const newTask = new Task({
+  description: "Play with my brother",
+  combleted: false,
+});
+
+newTask
+  .save()
+  .then(() => console.log(newTask))
+  .catch((err) => console.log(err));
