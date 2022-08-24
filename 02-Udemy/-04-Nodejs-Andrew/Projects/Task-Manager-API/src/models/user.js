bbc08cf088f1432bc20223c331c872746e3b65dc
salahcst344 <salahcst344@gmail.com>
2022-08-24 15:53:49 +0200

    Add login router

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/models/user.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -38,6 +39,19 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.statics.findByCredentials = async function (email, password) {
+  const user = await User.findOne({ email });
+
+  if (!user) throw new Error("Unable to login");
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) throw new Error("Unable to login");
+
+  return user;
+};
+
+// Hash the plain text password before saving
 userSchema.pre("save", async function (next) {
   const user = this;
 
