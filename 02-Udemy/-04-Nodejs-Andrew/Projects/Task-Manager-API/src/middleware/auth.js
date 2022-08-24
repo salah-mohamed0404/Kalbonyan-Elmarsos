513df9aa6a757663ec4793a3d0299a43383e160a
salahcst344 <salahcst344@gmail.com>
2022-08-24 21:07:28 +0200

    Add middleware auth

---
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const auth = async function (req, res, next) {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decooded = jwt.verify(token, "thisismynewcourse");
+    const user = await User.findOne({
+      _id: decooded._id,
+      "tokens.token": token,
+    });
+
+    if (!user) throw new Error();
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).send({ error: "Please authnticate." });
+  }
+};
+
+module.exports = auth;
