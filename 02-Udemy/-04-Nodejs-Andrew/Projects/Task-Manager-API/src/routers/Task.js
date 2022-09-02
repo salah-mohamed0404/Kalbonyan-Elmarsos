d1d6bff100ff5aaa1892835aded9fc2d0b144d95
salahcst344 <salahcst344@gmail.com>
2022-09-02 16:02:01 +0200

    Implementing tasks sorting

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/routers/Task.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/routers/Task.js
@@ -18,14 +18,25 @@ router.post("/tasks", auth, async function (req, res) {
 // Challenge
 router.get("/tasks", auth, async function (req, res) {
   const match = {};
+  const sort = {};
 
   if (req.query.completed) match.completed = req.query.completed === "true";
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+  }
 
   try {
     await req.user
       .populate({
         path: "tasks",
         match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
       })
       .execPopulate();
     res.send(req.user.tasks);
