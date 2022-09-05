539d2aafebaaa7ad1bd1546910b4bf0d727f355a
salahcst344 <salahcst344@gmail.com>
2022-09-05 14:32:29 +0200

    Add env vars

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/middleware/auth.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/src/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/user");
 const auth = async function (req, res, next) {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    const decooded = jwt.verify(token, "thisismynewcourse");
+    const decooded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({
       _id: decooded._id,
       "tokens.token": token,
