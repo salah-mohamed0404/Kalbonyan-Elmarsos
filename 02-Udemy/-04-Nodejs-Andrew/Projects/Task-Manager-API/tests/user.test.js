22a4fdb7886c17d3a2700b673c11211a5f37ae87
salahcst344 <salahcst344@gmail.com>
2022-09-05 15:35:59 +0200

    Add auth test

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/tests/user.test.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/tests/user.test.js
@@ -1,11 +1,20 @@
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
+const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
+  _id: userOneId,
   name: "mike",
   email: "mike@example.com",
   password: "56what!!",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+    },
+  ],
 };
 
 beforeEach(async () => {
@@ -42,4 +51,16 @@ test("should not login nonexistent user", async () => {
       password: "1235495",
     })
     .expect(400);
+});
+
+test("should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("should not get profile for unauthenticated user", async () => {
+  await request(app).get("/users/me").send().expect(401);
 });
