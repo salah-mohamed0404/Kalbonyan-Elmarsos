ef81c2f492163ead472f9c019c75bf97fe01ea4a
salahcst344 <salahcst344@gmail.com>
2022-09-06 20:54:54 +0200

    Add functionalty to share location

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Chat-App/public/js/chat.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Chat-App/public/js/chat.js
@@ -5,7 +5,25 @@ socket.on("message", (message) => console.log(message));
 document.getElementById("message-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const message = e.target.elements.message;
+  const message = e.target.elements.message.value;
 
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (error) =>
+    error ? console.log(error) : console.log("message was delivered!")
+  );
+});
+
+document.getElementById("send-location").addEventListener("click", () => {
+  if (!navigator.geolocation)
+    return alert("Goelocation is not supported by your browser.");
+
+  navigator.geolocation.getCurrentPosition((postion) =>
+    socket.emit(
+      "sendLocation",
+      {
+        lat: postion.coords.latitude,
+        long: postion.coords.longitude,
+      },
+      () => console.log("Location shared!")
+    )
+  );
 });
--- 02-Udemy/-04-Nodejs-Andrew/Projects/Chat-App/src/index.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Chat-App/src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -16,8 +17,28 @@ io.on("connection", (socket) => {
   console.log("New WebSocket connection");
 
   socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined!");
 
-  socket.on("sendMessage", (message) => io.emit("message", message));
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback("profanity is not allowed!");
+    }
+
+    io.emit("message", message);
+    callback();
+  });
+
+  socket.on("sendLocation", (coords, callback) => {
+    io.emit(
+      "message",
+      `https://www.google.com/maps?q=${coords.lat},${coords.long}`
+    );
+    callback();
+  });
+
+  socket.on("disconnect", () => io.emit("message", "A user has left!"));
 });
 
 server.listen(port, () => console.log(`Server is up on port ${port}!`));
