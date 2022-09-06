18323bcc59afd9c87ee55960661b7e01afad6dea
salahcst344 <salahcst344@gmail.com>
2022-09-06 18:38:09 +0200

    Coding challenge

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Chat-App/src/index.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Chat-App/src/index.js
@@ -1,11 +1,21 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirPath));
 
-app.listen(port, () => console.log(`Server is up on port ${port}!`));
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+
+  socket.emit("message", "Welcome!");
+});
+
+server.listen(port, () => console.log(`Server is up on port ${port}!`));
