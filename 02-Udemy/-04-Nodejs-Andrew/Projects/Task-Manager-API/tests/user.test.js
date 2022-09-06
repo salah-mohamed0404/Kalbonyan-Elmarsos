0dfab8c5661ba55296bfd6b3618469fe5a29c510
salahcst344 <salahcst344@gmail.com>
2022-09-06 14:52:19 +0200

    Grab shared code to anther file

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/tests/user.test.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Task-Manager-API/tests/user.test.js
@@ -1,26 +1,9 @@
 const request = require("supertest");
-const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
 const app = require("../src/app");
 const User = require("../src/models/user");
+const { userOneId, userOne, setupDatabase } = require("./fixtures/db");
 
-const userOneId = new mongoose.Types.ObjectId();
-const userOne = {
-  _id: userOneId,
-  name: "mike",
-  email: "mike@example.com",
-  password: "56what!!",
-  tokens: [
-    {
-      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
-    },
-  ],
-};
-
-beforeEach(async () => {
-  await User.deleteMany();
-  await new User(userOne).save();
-});
+beforeEach(setupDatabase);
 
 test("should signup an new user", async () => {
   const response = await request(app)
@@ -107,7 +90,7 @@ test("should upload avatar image", async () => {
 });
 
 test("should update valid user fields", async () => {
-  const response = await request(app)
+  await request(app)
     .patch("/users/me")
     .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
     .send({
