2c69528488ca2c0e263ea1b422357c136b4a8819
salahcst344 <salahcst344@gmail.com>
2022-09-06 18:46:47 +0200

    Small improvment

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Chat-App/public/js/chat.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Chat-App/public/js/chat.js
@@ -5,7 +5,7 @@ socket.on("message", (message) => console.log(message));
 document.getElementById("message-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const message = document.querySelector("input").value;
+  const message = e.target.elements.message;
 
   socket.emit("sendMessage", message);
 });
