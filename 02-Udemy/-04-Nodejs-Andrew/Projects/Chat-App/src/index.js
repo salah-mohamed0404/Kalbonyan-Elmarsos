5561ad527ba4ffe282476de413c228414fe03a38
salahcst344 <salahcst344@gmail.com>
2022-09-06 18:45:07 +0200

    Coding challenge

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Chat-App/src/index.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Chat-App/src/index.js
@@ -16,6 +16,8 @@ io.on("connection", (socket) => {
   console.log("New WebSocket connection");
 
   socket.emit("message", "Welcome!");
+
+  socket.on("sendMessage", (message) => io.emit("message", message));
 });
 
 server.listen(port, () => console.log(`Server is up on port ${port}!`));
